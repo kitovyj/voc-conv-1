8f57e8111c2e8016beb5c027336c0170b767698f
Alexander Ivanenko <Alexander Ivanenko>
2017-01-03 23:12:49 +0300

    dropout script improved

--- classifier/test-dropout.sh
+++ classifier/test-dropout.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in 0.0 0.25 0.5 0.75
 do
-   python3 -u ./voc-conv.py --fc-size 128 --dropout $i
+   python3 -u ./voc-conv.py --initial-weights-seed 1 --fc-size 128 --dropout $i
 done
