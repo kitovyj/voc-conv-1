b21b1d8c408a549ae591431460ad5db75bebd7ea
Alexander Ivanenko <alexander.ivanenko@gmail.com>
2018-10-07 21:00:16 +0300

    analyze deconv

---
+++ exploration/analyze_deconvolutions.m
@@ -0,0 +1,42 @@
+function analyze_deconvolutions()
+        
+    %format long;
+    format;
+
+    load('deconv_original_correlations_female.mat', 'female'); 
+    load('deconv_original_correlations_male.mat', 'male'); 
+
+    disp(mean(female(1:end, 2:end)))
+    disp(std(female(1:end, 2:end)) / sqrt(length(female)))    
+    disp(mean(male(1:end, 2:end)))
+    disp(std(male(1:end, 2:end)) / sqrt(length(male)))   
+    
+    sdata = csvread('features_fileid_sex_sexpredictiondnn_duration_avgfreq_avgvol_direction_peaks_breaks_broadband_tremolo_complex.csv');    
+
+    abnormal_f = female(abs(female(:, 8)) < 0.1, :);
+    disp(length(abnormal_f));
+    
+    %disp(abnormal_f);
+    
+    abnormal_m = male(abs(male(:, 8)) < 0.1, :);
+    disp(length(abnormal_m));    
+           
+    %disp(abnormal_m);
+    
+    %disp(abnormal_m(:, 1));
+
+    a_sdata_f = sdata(ismember(sdata(:, 1), abnormal_f(:, 1)), :);
+
+    %disp(a_sdata_f);
+    disp(abnormal_f(:, 1));
+    %disp(ismember(sdata(:, 1), abnormal_f(:, 1)));
+    
+    correct_abnormal_f = a_sdata_f(a_sdata_f(:, 3) == a_sdata_f(:, 4));
+    disp(size(correct_abnormal_f, 1) / size(abnormal_f, 1));
+        
+    a_sdata_m = sdata(ismember(sdata(:, 1), abnormal_m(:, 1)), :);
+    correct_abnormal_m = a_sdata_m(a_sdata_m(:, 3) == a_sdata_m(:, 4));
+    disp(size(correct_abnormal_m, 1) / size(abnormal_m, 1));
+
+    
+    
---
+++ exploration/process_deconvolutions.m
@@ -0,0 +1,109 @@
+function process_deconvolutions()
+
+
+    sdata = csvread('features_fileid_sex_sexpredictiondnn_duration_avgfreq_avgvol_direction_peaks_breaks_broadband_tremolo_complex.csv');    
+
+    data_path = './deconv/';
+    
+    dirs = dir(data_path);    
+
+    % process the corellation of neurons in the individual layers 
+    
+    
+    layer_folders = { 'relu', 'relu_1', 'relu_2', 'relu_3', 'relu_4', 'relu_5', 'fc_0', 'fc_1', 'fc_2' };
+        
+    image_size = 100;
+     
+    male = [];
+    female = [];
+    
+    count = 1;
+    
+    for folder = dirs'
+
+        if(folder.name == "." || folder.name == "..")
+           continue
+        end
+
+        [~, fname, ~] = fileparts(folder.name);
+        id = str2double(fname);
+        
+        cc = [id];
+             
+        for n = 1:numel(layer_folders)
+            
+            %disp(layer_folders{n});
+            
+            image_fn = strcat(data_path, folder.name, '/', layer_folders{n}, '/deconvolution/grid_image.png');                
+            image = imread(image_fn);
+             
+            % first six layers are convolution layers having 256 neurons
+            % each
+            if(n < 7)                
+                % convolutional layers are arranged in 16x16 grids                
+                grid_size = 16;                                                                
+            else                
+                grid_size = 11;
+            end
+                        
+            data = [];
+            
+            % disp(n);
+            % disp(grid_size);
+            
+            for i = 1:grid_size
+                for j = 1:grid_size
+                                
+                    % omit the last image for fully connected laters
+                    if(n >= 7 && (j == grid_size && i == grid_size))
+                        continue
+                    end
+                    
+                    % take a 1-pixel width separator line into accaunt
+                    y = (j - 1) * (image_size + 1) + 1;
+                    x = (i - 1) * (image_size + 1) + 1;
+                        
+                    %disp(x);
+                    %disp(y);
+                    
+                    
+                    sub_image = image(y:(y + image_size - 1), x:(x + image_size - 1), :);                        
+                    % flatten
+                    d = double(reshape(sub_image, 1, []));                        
+                      
+                    data = [data; d];
+                        
+                end
+            end                                
+                   
+            d = corr(data');
+            %DUnique = tril(d,-1)
+            I = tril(logical(ones(size(d))), -1);
+            ltd = d(I);
+            c = nanmean(ltd);
+            
+            cc = [cc c];
+                                    
+        end
+
+        disp(count)
+        count = count + 1;
+        disp(cc)
+        
+        row = sdata(sdata(:, 1) == id, :);
+        sex = row(3);
+        if(sex == 0)            
+           female = [female; cc];
+        else            
+           male = [male; cc];            
+        end
+        
+    end
+
+    disp(mean(female))
+    disp(std(female) / sqrt(length(female)))    
+    disp(mean(male))
+    disp(std(male) / sqrt(length(male)))   
+        
+    save('deconv_inner_correlations_female', 'female'); 
+    save('deconv_inner_correlations_male', 'male'); 
